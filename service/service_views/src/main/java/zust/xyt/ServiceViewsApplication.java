ed9f7964bfa10637f85268e50e2f1fb4ff1aeaff
andrew <andrew@zust.com>
2020-06-19 20:58:22 +0800

    service-views

---
+++ service/service_views/src/main/java/zust/xyt/ServiceViewsApplication.java
@@ -0,0 +1,16 @@
+package zust.xyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author AndrewElvis
+ * @date 2020-06-19-20:32
+ * @description
+ */
+@SpringBootApplication
+public class ServiceViewsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceViewsApplication.class, args);
+    }
+}
