aa30f5a5c2724aed7185bc9a7c07f464c38b6189
andrew <andrew@zust.com>
2020-06-27 17:21:26 +0800

    video backend finished

--- service/service_views/src/main/java/zust/xyt/ServiceViewsApplication.java
+++ service/service_views/src/main/java/zust/xyt/ServiceViewsApplication.java
@@ -1,7 +1,13 @@
 package zust.xyt;
 
+import com.sun.org.apache.regexp.internal.RE;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author AndrewElvis
@@ -9,8 +15,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @description
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ServiceViewsApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceViewsApplication.class, args);
+    }
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
---
+++ service/service_views/src/main/java/zust/xyt/entity/vo/UserQuery.java
@@ -0,0 +1,33 @@
+package zust.xyt.entity.vo;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @author Er1cWang
+ * @create 2020-06-18-16:50
+ */
+
+@Data
+public class UserQuery {
+    @ApiModelProperty(value = "用户名称,模糊查询")
+    private String name;
+
+    @ApiModelProperty(value = "用户性别")
+    private Integer sex;
+
+    @ApiModelProperty(value = "用户昵称")
+    private String nickname;
+
+    @ApiModelProperty(value = "查询开始时间", example = "2019-01-01 10:10:10")
+    private String begin;//注意，这里使用的是String类型，前端传过来的数据无需进行类型转换
+
+    @ApiModelProperty(value = "查询结束时间", example = "2019-12-01 10:10:10")
+    private String end;
+
+    @ApiModelProperty(value = "最少粉丝数量")
+    private int fansMinNum;
+
+    @ApiModelProperty(value = "最少关注数量")
+    private int subscribeMinNum;
+}
