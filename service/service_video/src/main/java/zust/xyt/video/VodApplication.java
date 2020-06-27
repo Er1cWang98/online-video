f8e22f63f8318685befa219831eea7d8b273feeb
andrew <andrew@zust.com>
2020-06-27 21:42:59 +0800

    single-video.html home.html

--- service/service_video/src/main/java/zust/xyt/video/VodApplication.java
+++ service/service_video/src/main/java/zust/xyt/video/VodApplication.java
@@ -2,7 +2,7 @@ package zust.xyt.video;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @ComponentScan(basePackages = {"zust.xyt"})
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class VodApplication {
 
     public static void main(String[] args) {
---
+++ service/service_views/src/main/java/zust/xyt/controller/VideoController.java
@@ -0,0 +1,52 @@
+package zust.xyt.controller;
+
+import org.bouncycastle.math.raw.Mod;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+import zust.xyt.ResponseResult;
+import zust.xyt.entity.User;
+import zust.xyt.entity.Video;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+/**
+ * @author AndrewElvis
+ * @date 2020-06-27-18:48
+ * @description
+ */
+@Controller
+@RequestMapping("/views/video")
+public class VideoController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/{id}")
+    public String singleVideo(@PathVariable String id, Model model) {
+        ResponseResult res = restTemplate.getForObject("http://SERVICE-VIDEO/vod/getPlayUrl/" + id,
+                ResponseResult.class);
+        model.addAttribute("url", res.getData().get("url"));
+        return "single-video";
+    }
+
+    @GetMapping("/toHome")
+    public String home(Model model) {
+        List<LinkedHashMap> result = restTemplate.getForObject("http://SERVICE-VIDEO/video", List.class);
+        List<User> userList = new ArrayList<>();
+        for (LinkedHashMap video : result) {
+            System.out.println(video.get("userId"));
+            User user = restTemplate.getForObject("http://SERVICE-USER/user" + video.get("userId"), User.class);
+            userList.add(user);
+        }
+        model.addAttribute("videoList", result);
+        model.addAttribute("userList", userList);
+        return "home";
+    }
+}
